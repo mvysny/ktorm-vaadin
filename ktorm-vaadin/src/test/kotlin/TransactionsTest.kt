e103a4fdb3caf577ebd16dbd25bc76a529fee11d
Martin Vysny <mavi@vaadin.com>
2025-12-10 16:30:04 +0200

    transactions: more tests

--- ktorm-vaadin/src/test/kotlin/TransactionsTest.kt
+++ ktorm-vaadin/src/test/kotlin/TransactionsTest.kt
@@ -7,27 +7,46 @@ import org.junit.jupiter.api.assertThrows
 import kotlin.test.expect
 
 class TransactionsTest : AbstractDbTest() {
-    @BeforeEach fun prepareTestData() {
+    @BeforeEach
+    fun prepareTestData() {
         db {
             ddl("create table foo (id int not null primary key auto_increment, name varchar(255))")
         }
     }
-    @AfterEach fun tearDownTestData() {
+
+    @AfterEach
+    fun tearDownTestData() {
         db {
             ddl("drop table if exists foo")
         }
     }
 
-    @Test fun dbCommits() {
+    @Test
+    fun dbCommits() {
         db {
             ddl("insert into foo (name) values ('foo')")
         }
         db {
-            expect(1) { sql("select count(*) from foo") { row -> row.getInt(1) } .first() }
+            expect(1) { sql("select count(*) from foo") { row -> row.getInt(1) }.first() }
         }
     }
 
-    @Test fun dbRollsBack() {
+    @Test
+    fun nestedTransactions() {
+        db {
+            db {
+                db {
+                    ddl("insert into foo (name) values ('foo')")
+                }
+            }
+        }
+        db {
+            expect(1) { sql("select count(*) from foo") { row -> row.getInt(1) }.first() }
+        }
+    }
+
+    @Test
+    fun dbRollsBack() {
         assertThrows<RuntimeException> {
             db {
                 ddl("insert into foo (name) values ('foo')")
@@ -35,7 +54,7 @@ class TransactionsTest : AbstractDbTest() {
             }
         }
         db {
-            expect(0) { sql("select count(*) from foo") { row -> row.getInt(1) } .first() }
+            expect(0) { sql("select count(*) from foo") { row -> row.getInt(1) }.first() }
         }
     }
 }
