82ac9a9f9f17c7253b1986f2ba3058c7a10e8f6d
Martin Vysny <mavi@vaadin.com>
2025-12-10 21:51:55 +0200

    testapp db: create indices

--- testapp/src/main/resources/db/migration/V01__CreateDepartment.sql
+++ testapp/src/main/resources/db/migration/V01__CreateDepartment.sql
@@ -4,3 +4,5 @@ create table t_department
     name     varchar(128) not null,
     location varchar(128) not null
 );
+create unique index idx_department_name ON t_department(name);
+create index idx_department_location ON t_department(location);
--- testapp/src/main/resources/db/migration/V02__CreateEmployee.sql
+++ testapp/src/main/resources/db/migration/V02__CreateEmployee.sql
@@ -8,3 +8,10 @@ create table t_employee
     salary        bigint       not null,
     department_id int          not null
 );
+
+create index idx_e_name on t_employee(name);
+create index idx_e_job on t_employee(job);
+create index idx_e_manager on t_employee(manager_id);
+create index idx_e_hire_date on t_employee(hire_date);
+create index idx_e_salary on t_employee(salary);
+create index idx_e_depid on t_employee(department_id);
