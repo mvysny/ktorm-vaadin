4012240210166468f962ed68990b0661bf8251a4
Martin Vysny <mavi@vaadin.com>
2025-12-11 09:11:10 +0200

    add tests for bind

---
+++ ktorm-vaadin/src/test/kotlin/BinderTests.kt
@@ -0,0 +1,43 @@
+package com.github.mvysny.ktormvaadin
+
+import com.vaadin.flow.component.textfield.IntegerField
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.binder.BeanValidationBinder
+import org.junit.jupiter.api.Test
+import kotlin.test.expect
+
+class BinderTests {
+    /**
+     * Uses [bind] to bind fields to entity properties.
+     */
+    class Form {
+        val nameField = TextField()
+        val ageField = IntegerField()
+        val binder = BeanValidationBinder(Person::class.java)
+
+        init {
+            binder.forField(nameField).bind(Persons.name)
+            binder.forField(ageField).bind(Persons.age)
+        }
+    }
+
+    @Test
+    fun valuePropagatedFromBeanToForm() {
+        val form = Form()
+        val person = Person { name = "Rimmer"; age = 35 }
+        form.binder.readBean(person)
+        expect("Rimmer") { form.nameField.value}
+        expect(35) { form.ageField.value}
+    }
+
+    @Test
+    fun valuesPropagatedFromFormToBean() {
+       val form = Form()
+       form.nameField.value = "Rimmer"
+       form.ageField.value = 35
+       val p = Person{}
+       form.binder.writeBean(p)
+       expect("Rimmer") { p.name }
+       expect(35) { p.age }
+    }
+}
